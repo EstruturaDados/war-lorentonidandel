3b92982bc4891ae0df57d9e823d2b91238fd1b01
lorentonidandel <130365972+lorentonidandel@users.noreply.github.com>
2026-06-28 20:17:29 -0300

    Update war.c

--- war.c
+++ war.c
@@ -41,10 +41,40 @@ int main() {
 
     printf("========================================\n");
     printf("      DESAFIO WAR ESTRUTURADO\n");
-    printf("        NIVEL NOVATO\n");
+    printf("          NIVEL NOVATO\n");
     printf("========================================\n");
 
     // Cadastro dos territórios
     for (int i = 0; i < TOTAL_TERRITORIOS; i++) {
 
-        printf("\n--- Cadastrando Territorio %d ---\n", i + 1
+        printf("\n--- Cadastrando Territorio %d ---\n", i + 1);
+
+        printf("Nome do territorio: ");
+        scanf("%29s", territorios[i].nome);
+
+        printf("Cor do exercito: ");
+        scanf("%9s", territorios[i].cor);
+
+        printf("Numero de tropas: ");
+        scanf("%d", &territorios[i].tropas);
+    }
+
+    // Exibição dos dados cadastrados
+    printf("\n========================================\n");
+    printf("        ESTADO ATUAL DO MAPA\n");
+    printf("========================================\n");
+
+    for (int i = 0; i < TOTAL_TERRITORIOS; i++) {
+
+        printf("\nTerritorio %d\n", i + 1);
+        printf("Nome...........: %s\n", territorios[i].nome);
+        printf("Cor do exercito: %s\n", territorios[i].cor);
+        printf("Tropas.........: %d\n", territorios[i].tropas);
+    }
+
+    printf("\n========================================\n");
+    printf("Cadastro concluido com sucesso!\n");
+    printf("========================================\n");
+
+    return 0;
+}
